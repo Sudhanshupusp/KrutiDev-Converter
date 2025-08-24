8efd6ef594769f3bf3fe0f70fe74169e046b0fb3
Sudhanshupusp <pusps747@gmail.com>
2025-08-24 17:47:45 +0530

    Add cors origin check

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # your frontend URL
+    allow_origins=["*"],  # your frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
--- frontend/app/upload/page.tsx
+++ frontend/app/upload/page.tsx
@@ -23,7 +23,7 @@ export default function UploadPage() {
     setDownloadUrl(null);
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/convert", {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/convert`, {
         method: "POST",
         body: formData,
       });
